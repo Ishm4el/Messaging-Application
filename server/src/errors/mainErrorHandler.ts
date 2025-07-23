7e1181324dc06d168c4e120331559ca3a3a43899
Ismael <ismaelv@protonmail.com>
2025-07-23 16:13:56 -0700

    Updated mainErrorHandler.ts so that the default export can now handle various errors; viz. ExpressError, Error, and an else scenario.

--- server/src/errors/mainErrorHandler.ts
+++ server/src/errors/mainErrorHandler.ts
@@ -2,16 +2,23 @@ import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import ExpressError from "./ExpressError";
 
 const mainErrorHandler: ErrorRequestHandler = (
-  err: ExpressError,
+  err: ExpressError | Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.error(err);
-  res
-    .status(err.statusCode || 500)
-    .json({ status: err.statusCode, message: err.message });
-  return;
+  if (err instanceof ExpressError) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ status: err.statusCode, message: err.message, name: err.name });
+    return;
+  } else if (err instanceof Error) {
+    console.error(err);
+    res.status(500).json({ status: 500, message: JSON.stringify(err) });
+  } else {
+    res.status(500).json({ status: 500, message: "ERROR" });
+  }
 };
 
 export default mainErrorHandler;
