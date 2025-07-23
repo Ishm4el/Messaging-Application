630847e5f0d219e0bc83950f676c2a31543f6936
Ismael <ismaelv@protonmail.com>
2025-07-23 13:07:20 -0700

    Extracted the onSubmit form handler in the Login.tsx file

--- client/src/routes/LogIn.tsx
+++ client/src/routes/LogIn.tsx
@@ -8,49 +8,46 @@ export default function LogIn() {
   const outProps: {
     setLogged: React.Dispatch<React.SetStateAction<boolean>>;
   } = useOutletContext();
+
+  const formHandlerLogin = async (event) => {
+    event.preventDefault();
+    // alert(email + " " + password);
+    const response = await fetch("http://localhost:3000/authorization/log_in", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+      credentials: "include",
+    });
+    const status = response.status;
+    const res = await response.json();
+    console.log("finished the login!");
+
+    console.log(response);
+    console.log(res);
+
+    if (status === 401) {
+      if (res.message === "username") alert("No matching username");
+      if (res.message === "password") alert("Incorrect password");
+    }
+
+    if (status === 200) {
+      localStorage.setItem("username", res.username);
+      localStorage.setItem(
+        "backgroundColor",
+        // res.settings?.backgroundColorSettings || "main"
+        "main"
+      );
+      outProps.setLogged(true);
+      navigate("/friends");
+    }
+  };
+
   return (
     <main className={styles["main"]}>
       <section className={styles["section"]}>
         <h1 className={styles["header"]}>Logging In</h1>
-        <form
-          className={styles["form"]}
-          onSubmit={async (event) => {
-            event.preventDefault();
-            // alert(email + " " + password);
-            const response = await fetch(
-              "http://localhost:3000/authorization/log_in",
-              {
-                method: "POST",
-                mode: "cors",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ username, password }),
-                credentials: "include",
-              }
-            );
-            const status = response.status;
-            const res = await response.json();
-            console.log("finished the login!");
-
-            console.log(response);
-            console.log(res);
-
-            if (status === 401) {
-              if (res.message === "username") alert("No matching username");
-              if (res.message === "password") alert("Incorrect password");
-            }
-
-            if (status === 200) {
-              localStorage.setItem("username", res.username);
-              localStorage.setItem(
-                "backgroundColor",
-                // res.settings?.backgroundColorSettings || "main"
-                "main"
-              );
-              outProps.setLogged(true);
-              navigate("/friends");
-            }
-          }}
-        >
+        <form className={styles["form"]} onSubmit={formHandlerLogin}>
           <div className={styles["input-container"]}>
             <label htmlFor="username">Username:</label>
             <input
